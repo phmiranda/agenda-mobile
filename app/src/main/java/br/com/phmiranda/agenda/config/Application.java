4a88bc3dc04c1e6c68e8824c3494f9caf32dcd55
phmiranda <pehhagah.1607@gmail.com>
2020-05-29 02:54:45 -0300

    [add] - criação das constantes de configuração do App.

---
+++ app/src/main/java/br/com/phmiranda/agenda/config/Application.java
@@ -0,0 +1,15 @@
+/**
+ * User: phmiranda
+ * Project: agenda-mobile
+ * Date: 29/05/20
+ */
+
+package br.com.phmiranda.agenda.config;
+
+public class Application {
+    public final String APP_TITULO = "Agenda Eletrônica";
+    public final String APP_FORMULARIO = "Agenda Eletrônica";
+    public final String APP_FORMULARIO_CADASTRO = "Cadastro de Contato";
+    public final String APP_FORMULARIO_ATUALIZACAO = "Atualização de Contato";
+
+}
